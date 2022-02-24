27483893f395f7ab944454d516eeafdc762b860d
genskeu <uligenske@gmail.com>
2022-02-24 16:52:33 +0100

    deactivate unstable features before master merge

--- app/DBmodel.py
+++ app/DBmodel.py
@@ -31,7 +31,7 @@ class User(db.Model):
        results: links to results the user produced
     """
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(120), nullable=False)
     email = db.Column(db.String(60))
     access_level = db.Column(db.Integer(), nullable=False)
