08692b91d0a6d6dbe02fcd1188fc7314e5875e7a
genskeu <uligenske@gmail.com>
2022-06-14 20:02:40 +0200

    added dicomViewer and tailwind

--- frontend/src/components/dicomViewer/DicomViewer.vue
+++ frontend/src/components/dicomViewer/DicomViewer.vue
@@ -1,80 +1,66 @@
 <template>
-  <div
-    class="relative grid grid-rows-6 grid-cols-1"
-    @cornerstoneimagerendered.capture="displayStackIndex"
-  >
+  <div class='relative grid grid-rows-6 grid-cols-1' @cornerstoneimagerendered.capture='displayStackIndex'>
     <!-- image viewer -->
-    <div
-      ref="viewer"
-      class="dicom_viewer row-span-5 col-span-1 relative"
-    >
+    <div ref='viewer' class='dicom_viewer row-span-5 col-span-1 relative'>
       <!-- metadata viewer -->
-      <div class="absolute top-0 left-0 p-4 text-white">
-        <ul class="list-none text-left">
-          <li v-for="(metadata, index) in stack_meta_data.tl" :key="index">
+      <div class='absolute top-0 left-0 p-4 text-white'>
+        <ul class='list-none text-left'>
+          <li v-for='(metadata, index) in stack_meta_data.tl' :key='index'>
             {{ metadata.tag }} {{ metadata.value }}
           </li>
         </ul>
       </div>
-      <div class="absolute top-0 right-0 p-4 text-white">
-        <ul class="list-none text-right">
-          <li v-for="(metadata, index) in stack_meta_data.tr" :key="index">
+      <div class='absolute top-0 right-0 p-4 text-white'>
+        <ul class='list-none text-right'>
+          <li v-for='(metadata, index) in stack_meta_data.tr' :key='index'>
             {{ metadata.tag }} {{ metadata.value }}
           </li>
         </ul>
       </div>
-      <div class="absolute bottom-0 left-0 p-4 text-white">
-        <ul id="viewer_bl" class="list-none text-left">
-          <li ref="slice_index"></li>
-          <li v-for="(metadata, index) in stack_meta_data.bl" :key="index">
+      <div class='absolute bottom-0 left-0 p-4 text-white'>
+        <ul id='viewer_bl' class='list-none text-left'>
+          <li ref='slice_index'></li>
+          <li v-for='(metadata, index) in stack_meta_data.bl' :key='index'>
             {{ metadata.tag }} {{ metadata.value }}
           </li>
         </ul>
       </div>
-      <div class="absolute bottom-0 right-0 p-4 text-white">
-        <ul class="list-none text-right">
-          <li v-for="(metadata, index) in stack_meta_data.br" :key="index">
+      <div class='absolute bottom-0 right-0 p-4 text-white'>
+        <ul class='list-none text-right'>
+          <li v-for='(metadata, index) in stack_meta_data.br' :key='index'>
             {{ metadata.tag }} {{ metadata.value }}
           </li>
         </ul>
       </div>
     </div>
     <!-- image and mask selector -->
-    <div class="flex items-center row-span-1 col-span-1 animate-fade-in-up">
-      <div class="flex grow items-center mr-2">
-        <label
-          class="block text-sm font-medium text-gray-900 dark:text-gray-400"
-          >Select your Image:
+    <div class='flex items-center row-span-1 col-span-1 animate-fade-in-up'>
+      <div class='flex grow items-center mr-2'>
+        <label class='block text-sm font-medium text-gray-900 dark:text-gray-400'>Select your Image:
         </label>
-        <select
-          ref="image_select_id"
-          class="bg-gray-50 border border-gray-300 text-gray-900 grow text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          @change="loadStackOnSelect"
-        >
+        <select ref='image_select_id'
+          class='bg-gray-50 border border-gray-300 text-gray-900 grow text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          @change='loadStackOnSelect'>
           <option></option>
-          <option v-for="stack in stackNames.sort()" :key="stack">
+          <option v-for='stack in stackNames.sort()' :key='stack'>
             {{ stack }}
           </option>
         </select>
       </div>
-      <div class="flex grow items-center">
-        <label
-          class="block text-sm font-medium text-gray-900 dark:text-gray-400"
-          >Select your Mask:
+      <div class='flex grow items-center'>
+        <label class='block text-sm font-medium text-gray-900 dark:text-gray-400'>Select your Mask:
         </label>
 
-        <select
-          ref="mask_select_id"
-          class="bg-gray-50 border border-gray-300 text-gray-900 grow text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          @change="loadMaskOnSelect"
-        >
+        <select ref='maskSelect_id'
+          class='bg-gray-50 border border-gray-300 text-gray-900 grow text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+          @change='loadMaskOnSelect'>
           <option></option>
           <option disabled>Grundtruth Masks</option>
-          <option v-for="mask in gt_masks" :key="mask" mask-type="gt">
+          <option v-for='mask in gt_masks' :key='mask' mask-type='gt'>
             {{ mask }}
           </option>
           <option disabled>Pred Masks (output)</option>
-          <option v-for="mask in pred_masks" :key="mask" mask-type="pred">
+          <option v-for='mask in pred_masks' :key='mask' mask-type='pred'>
             {{ mask }}
           </option>
         </select>
@@ -84,270 +70,184 @@
 </template>
 
 <script>
-import Hammer from "hammerjs";
-import dicomParser from "dicom-parser";
-import cornerstoneMath from "cornerstone-math";
-import cornerstoneWADOImageLoader from "cornerstone-wado-image-loader";
-import cornerstone from "cornerstone-core";
-import cornerstoneTools from "cornerstone-tools";
-import axios from "axios";
+import Hammer from 'hammerjs'
+import dicomParser from 'dicom-parser'
+import cornerstoneMath from 'cornerstone-math'
+import cornerstoneWADOImageLoader from 'cornerstone-wado-image-loader'
+import cornerstone from 'cornerstone-core'
+import cornerstoneTools from 'cornerstone-tools'
+import axios from 'axios'
 
 // Cornerstone DICOM Viewer Setup
-cornerstoneWADOImageLoader.external.cornerstone = cornerstone;
-cornerstoneWADOImageLoader.external.dicomParser = dicomParser;
-cornerstoneTools.external.cornerstoneMath = cornerstoneMath;
-cornerstoneTools.external.cornerstone = cornerstone;
-cornerstoneTools.external.Hammer = Hammer;
+cornerstoneWADOImageLoader.external.cornerstone = cornerstone
+cornerstoneWADOImageLoader.external.dicomParser = dicomParser
+cornerstoneTools.external.cornerstoneMath = cornerstoneMath
+cornerstoneTools.external.cornerstone = cornerstone
+cornerstoneTools.external.Hammer = Hammer
 
 export default {
-  data() {
+  data () {
     return {
-      viewer_id: "test",
+      viewer_id: 'test',
       element: undefined,
-      stack_select_id: "test_sel",
+      stack_select_id: 'test_sel',
       stack_displayed: undefined,
       stack_meta_data: {
         tl: undefined,
         tr: undefined,
         bl: undefined,
-        br: undefined,
+        br: undefined
       },
       current_stack: undefined,
       stacks: [],
       stackNames: [],
-      mask_select_id: "test_mask_sel",
+      maskSelect_id: 'test_mask_sel',
       mask_displayed: undefined,
       gt_masks: [],
-      pred_masks: [],
-    };
+      pred_masks: []
+    }
   },
   computed: {
-    getMasknames() {
-      return this.gt_masks.concat(this.pred_masks);
-    },
+    getMasknames () {
+      return this.gt_masks.concat(this.pred_masks)
+    }
   },
   watch: {},
-  created() {
-    this.stacks = this.$store.state.input.stackData;
-    this.stackNames = this.$store.state.input.stackNames;
-    this.gt_masks = this.$store.state.input.gtmaskNames;
-    this.pred_masks = this.$store.state.output.predmaskNames;
+  created () {
+    // this.stacks = this.$store.state.input.stackData
+    // this.stackNames = this.$store.state.input.stackNames
+    // this.gt_masks = this.$store.state.input.gtmaskNames
+    // this.pred_masks = this.$store.state.output.predmaskNames
   },
-  mounted() {
-    this.initViewer();
-    var elements = cornerstone.getEnabledElements();
-    elements.forEach((ele) => cornerstone.resize(ele.element));
+  mounted () {
+    this.initViewer()
+    var elements = cornerstone.getEnabledElements()
+    elements.forEach((ele) => cornerstone.resize(ele.element))
   },
-  unmounted() {
-    var elements = cornerstone.getEnabledElements();
-    elements.forEach((ele) => cornerstone.resize(ele.element));
+  unmounted () {
+    var elements = cornerstone.getEnabledElements()
+    elements.forEach((ele) => cornerstone.resize(ele.element))
   },
   methods: {
-    initViewer() {
-      this.element = this.$refs.viewer;
+    initViewer () {
+      this.element = this.$refs.viewer
       cornerstone.enable(this.element, {
-        renderer: "webgl",
-      });
+        renderer: 'webgl'
+      })
 
       this.element.addEventListener(
-        "contextmenu",
+        'contextmenu',
         function (e) {
-          e.preventDefault();
+          e.preventDefault()
         },
         false
-      );
+      )
     },
-    loadMaskOnSelect(event) {
-      const segMaskName = event.target.value;
-      var segmentation = cornerstoneTools.getModule("segmentation");
-      const element = this.element;
-      segmentation.setters.activeLabelmapIndex(this.element, 0);
-      var labelmap3D = segmentation.getters.labelmap3D(element, 0);
+    loadMaskOnSelect (event) {
+      const segMaskName = event.target.value
+      var segmentation = cornerstoneTools.getModule('segmentation')
+      const element = this.element
+      segmentation.setters.activeLabelmapIndex(this.element, 0)
+      var labelmap3D = segmentation.getters.labelmap3D(element, 0)
       const masktype =
-        event.target.selectedOptions[0].getAttribute("mask-type");
-      var url = "http://localhost:5000/maskdata/" + masktype + "/" + segMaskName
-      if(segMaskName){
+        event.target.selectedOptions[0].getAttribute('mask-type')
+      var url = 'http://localhost:5000/maskdata/' + masktype + '/' + segMaskName
+      if (segMaskName) {
         axios.get(url).then((response) => {
-          var seg_data = response.data.maskData
-          labelmap3D.labelmaps2D = Array(seg_data.length);
-          seg_data.forEach(function (seg, index) {
+          var segData = response.data.maskData
+          labelmap3D.labelmaps2D = Array(segData.length)
+          segData.forEach(function (seg, index) {
             if (seg) {
               var l2dforImageIdIndex = {
                 pixelData: Uint16Array.from(JSON.parse(seg)),
-                segmentsOnLabelmap: [0, 1, 2],
-              };
-              labelmap3D.labelmaps2D.splice(index, 0, l2dforImageIdIndex);
+                segmentsOnLabelmap: [0, 1, 2]
+              }
+              labelmap3D.labelmaps2D.splice(index, 0, l2dforImageIdIndex)
             }
-          });
-        cornerstone.updateImage(element);
-      }).catch(error => {
-        alert("Mask not found!");
-        console.log(error)
-      })
+          })
+          cornerstone.updateImage(element)
+        }).catch(error => {
+          alert('Mask not found!')
+          console.log(error)
+        })
       } else {
-        labelmap3D.labelmaps2D = [];
-        cornerstone.updateImage(element);
+        labelmap3D.labelmaps2D = []
+        cornerstone.updateImage(element)
       }
     },
-    loadStackOnSelect(event) {
-      const stack_name = event.target.value;
-      if (stack_name != "") {
-        const data = this.$store.getters["input/getStackData"](stack_name);
-        const stacks = this.parseImagesCornerstone(data.data);
-        this.loadDisplayCornerstone(stacks[0], stacks[1], "input").then(() => {
-          this.setMetaData("input");
-          this.filterMasks(stack_name);
-        });
+    loadStackOnSelect (event) {
+      const stackName = event.target.value
+      if (stackName !== '') {
+        const data = this.$store.getters['input/getStackData'](stackName)
+        const stacks = this.parseImagesCornerstone(data.data)
+        this.loadDisplayCornerstone(stacks[0], stacks[1], 'input').then(() => {
+          this.setMetaData('input')
+          this.filterMasks(stackName)
+        })
       }
     },
-    filterMasks(volName) {
-      var mask_select = this.$refs.mask_select_id;
-      mask_select.value = "";
-      for (var option of mask_select) {
+    filterMasks (volName) {
+      var maskSelect = this.$refs.maskSelect_id
+      maskSelect.value = ''
+      for (var option of maskSelect) {
         if (
           option.value.includes(volName) |
-          option.value.includes("gt") |
-          (option.value == "") |
-          (option.disabled == true)
+          option.value.includes('gt') |
+          (option.value === '') |
+          (option.disabled === true)
         ) {
-          option.removeAttribute("hidden");
+          option.removeAttribute('hidden')
         } else {
-          option.setAttribute("hidden", "hidden");
+          option.setAttribute('hidden', 'hidden')
         }
       }
     },
-    parseImagesCornerstone(images) {
-      const scheme = "wadouri";
-      const baseUrl = "http://localhost:5000";
-      var data_type = "";
-      var folder = "";
-      data_type = "input";
-      folder = images[0].split("/")[0];
-      const series = [];
-      for (let image of images.sort()) {
-        series.push(image.split("/").pop());
+    parseImagesCornerstone (images) {
+      const scheme = 'wadouri'
+      const baseUrl = 'http://localhost:5000'
+      var dataType = ''
+      var folder = ''
+      dataType = 'input'
+      folder = images[0].split('/')[0]
+      const series = []
+      for (var image of images.sort()) {
+        series.push(image.split('/').pop())
       }
       const imageIds = series.map(
         (seriesImage) =>
-          `${scheme}:${baseUrl}/uploads/${data_type}/${folder}/${seriesImage}`
-      );
+         `${scheme}:${baseUrl}/uploads/${dataType}/${folder}/${seriesImage}`
+      )
       const stack = {
         currentImageIdIndex: 0,
-        imageIds,
-      };
-      return [imageIds, stack];
+        imageIds
+      }
+      return [imageIds, stack]
     },
-    loadDisplayCornerstone(imageIds, stack) {
+    loadDisplayCornerstone (imageIds, stack) {
       // load images and set the stack
       const promise = cornerstone.loadImage(imageIds[0]).then((image) => {
-        this.stack_displayed = image;
-        this.current_stack = stack;
-        cornerstone.displayImage(this.element, image);
-        cornerstoneTools.addStackStateManager(this.element, ["stack"]);
-        cornerstoneTools.addToolState(this.element, "stack", stack);
-      });
-      return promise;
+        this.stack_displayed = image
+        this.current_stack = stack
+        cornerstone.displayImage(this.element, image)
+        cornerstoneTools.addStackStateManager(this.element, ['stack'])
+        cornerstoneTools.addToolState(this.element, 'stack', stack)
+      })
+      return promise
     },
-    displayStackIndex() {
-      const stack = this.current_stack;
+    displayStackIndex () {
+      const stack = this.current_stack
       if (stack) {
-        var slice = this.$refs.slice_index;
+        var slice = this.$refs.slice_index
         slice.innerHTML =
-          `Image:` +
+          'Image:' +
           (stack.currentImageIdIndex + 1) +
-          `/` +
-          stack.imageIds.length;
+         '/' +
+          stack.imageIds.length
       }
-    },
-    setMetaData(imageType) {
-      const image = this.stack_displayed;
-      const payload_info = {
-        image: imageType,
-        data: [
-          cornerstone.metaData.get("generalSeriesModule", image.imageId),
-          cornerstone.metaData.get("patientStudyModule", image.imageId),
-          cornerstone.metaData.get("imagePlaneModule", image.imageId),
-        ],
-      };
-      const payload_viewer_tl = {
-        image: imageType,
-        data: [
-          { tag: "", value: image.data.string("x00100010") },
-          { tag: "", value: image.data.string("x00100020") },
-          { tag: "", value: image.data.string("x00081030") },
-          { tag: "", value: image.data.string("x0008103e") },
-        ],
-      };
-      const payload_viewer_tr = {
-        image: imageType,
-        data: [
-          { tag: "", value: image.data.string("x00080080") },
-          { tag: "", value: image.data.string("x00081090") },
-          { tag: "", value: image.data.string("x00080090") },
-        ],
-      };
-      if (
-        image.data.string("x00200011") &&
-        image.data.string("x00180050") &&
-        image.data.string("x00201041")
-      ) {
-        var payload_viewer_bl = {
-          image: imageType,
-          data: [
-            { tag: "Series:", value: image.data.string("x00200011") },
-            {
-              tag: "",
-              value: `ST: ${image.data.string(
-                "x00180050"
-              )} SL: ${image.data.string("x00201041")}`,
-            },
-          ],
-        };
-      } else {
-        payload_viewer_bl = { image: imageType, data: [] };
-      }
-
-      if (
-        image.data.string("x00181151") &&
-        image.data.string("x00281051") &&
-        image.data.string("x00281051")
-      ) {
-        var payload_viewer_br = {
-          image: imageType,
-          data: [
-            {
-              tag: "",
-              value: `${image.data.string("x00181151")} mA ${image.data.string(
-                "x00180060"
-              )} kV`,
-            },
-            {
-              tag: "",
-              value: `WL: ${image.data.string(
-                "x00281051"
-              )} WW: ${image.data.string("x00281051")}`,
-            },
-          ],
-        };
-      } else {
-        payload_viewer_br = { image: imageType, data: [] };
-      }
-      // send to store to display in infosection
-      this.$store.dispatch("viewer/saveMetadata", {
-        payload_info,
-        payload_viewer_tl,
-        payload_viewer_tr,
-        payload_viewer_bl,
-        payload_viewer_br,
-      });
-
-      this.stack_meta_data.tl = payload_viewer_tl.data;
-      this.stack_meta_data.tr = payload_viewer_tr.data;
-      this.stack_meta_data.bl = payload_viewer_bl.data;
-      this.stack_meta_data.br = payload_viewer_br.data;
-    },
-  },
-};
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+</style>
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,5 +4,6 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap'
+import './index.css'
 
 createApp(App).use(store).use(router).mount('#app')
