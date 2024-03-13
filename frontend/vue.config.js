a557aa2fce4fa7a5b886d41080620d1787e91676
genskeu <uligenske@gmail.com>
2024-03-13 19:00:21 +0100

    update vue config for development under windows

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,20 +1,28 @@
 // vue.config.js
 module.exports = {
   configureWebpack : {
-    devtool: 'source-map'
-  },
-  devServer: {
-    allowedHosts: 'all',
-    proxy: {
-      '^/flask-api': {
-        target: 'http://backend:5000',
-        ws: true,
-        changeOrigin: true,
-        pathRewrite: {
-          '^/flask-api': '/'
-        }
-      }
-    }
+    devtool: 'source-map',
+    entry: "./src/main.js",
+    devServer: {
+        allowedHosts: 'all',
+        proxy: {
+          '^/flask-api': {
+            target: 'http://backend:5000',
+            ws: true,
+            changeOrigin: true,
+            pathRewrite: {
+              '^/flask-api': '/'
+            }
+          }
+        },
+        // hot reload windows issue https://stackoverflow.com/questions/71817054/dockerized-vue-app-hot-reload-does-not-work
+        hot: true
+    },
+    watch: true,
+    watchOptions: {
+        ignored: /node_modules/,
+        poll: 1000,
+    },
   },
   transpileDependencies: true
 }
